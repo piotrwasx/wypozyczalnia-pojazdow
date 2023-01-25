157d4761546b4dd2d2741eec385859c0adfeca4f
piotrwas <ns.piotr@gmail.com>
2023-01-25 20:05:45 +0100

    Initial Commit

---
+++ wypozyczalnia-pojazdow/wypozyczalnia_pojazdowApp.swift
@@ -0,0 +1,17 @@
+//
+//  wypozyczalnia_pojazdowApp.swift
+//  wypozyczalnia-pojazdow
+//
+//  Created by Piotr Waś on 25/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct wypozyczalnia_pojazdowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
